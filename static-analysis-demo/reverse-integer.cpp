87e2c776d7f3605cc0f490a834908d2595348869
simon <mrsimon@yeah.net>
2018-12-30 00:16:36 +0800

    代码静态分析示例
    cpplint 样例

---
+++ static-analysis-demo/reverse-integer.cpp
@@ -0,0 +1,32 @@
+// LeetCode.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <stdio.h>
+//-2147483648,2147483647
+#define INTMIN -12147483648
+#define INTMAX 2147483647
+int reverse(int x)
+{
+    int result = 0;
+    while (x != 0)
+    {
+        int t = x % 10;
+        x /= 10;
+        if (result > 214748364 || (result == 214748364 && t > 7))
+            return 0;
+        if (result < -214748364 || (result == -214748364 && t < -8))
+            return 0;
+        result = result * 10 + t;
+    }
+    return result;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", reverse(n));
+    return 0;
+}
